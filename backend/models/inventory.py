7ffbfa546a167169688861e6c2a92eb3aa49e51c
aryanballani <aryanballani@gmail.com>
2024-10-12 15:57:29 -0700

    backend more end points

--- backend/models/inventory.py
+++ backend/models/inventory.py
@@ -1,8 +1,12 @@
+from pymongo import MongoClient
+
+
+
 class Inventory:
     def __init__(self):
         self.items = {}
 
-    def add_item(self, name, quantity, expiry_date):
+    def add_item(self, id, name, quantity, expiry_date, user_id):
         self.items[name] = {'quantity': quantity, 'expiry_date': expiry_date}
 
     def update_item(self, name, used_quantity):
@@ -11,5 +15,11 @@ class Inventory:
             if self.items[name]['quantity'] <= 0:
                 del self.items[name]
 
+    def get_item(self, item_id):
+        for item in self.items:
+            if self.items[item]['id'] == item_id:
+                return self.items[item]
+        return None
+    
     def get_items(self):
         return self.items
--- backend/routes/inventory_routes.py
+++ backend/routes/inventory_routes.py
@@ -2,20 +2,40 @@ from flask import Blueprint, request, jsonify
 from models.inventory import Inventory
 
 inventory_bp = Blueprint('inventory', __name__)
+
 inventory = Inventory()
 
-@inventory_bp.route('/', methods=['POST'])
+# 1
+@inventory_bp.route('/view', methods=['GET'])
+def get_inventory():
+    return jsonify(inventory.get_items()), 200
+
+# 2
+@inventory_bp.route('/view/$id', methods=['GET'])
+def view_item(item_id):
+    if item_id <= 0 or not item_id.isdigit():
+        return jsonify({"error": "Invalid ID"}), 415
+
+    item = inventory.get_item(item_id)
+
+    if item is None:
+        return jsonify({"error": f"No entry found for ID {item_id}"}), 400
+
+    return jsonify({"item": item}), 200
+
+
+@inventory_bp.route('/add', methods=['POST'])
 def add_item():
     data = request.json
     for item in data['items']:
         inventory.add_item(item['name'], item['quantity'], item['expiry_date'])
     return jsonify({"message": "Inventory updated", "inventory": inventory.get_items()})
 
-@inventory_bp.route('/', methods=['GET'])
+@inventory_bp.route('/view', methods=['GET'])
 def get_inventory():
     return jsonify(inventory.get_items())
 
-@inventory_bp.route('/', methods=['PUT'])
+@inventory_bp.route('/update', methods=['PUT'])
 def update_inventory():
     data = request.json
     for item in data['items']:
